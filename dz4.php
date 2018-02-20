d38043b8bbc1db08cde5e0a38a0669e9fb39fe71
vollva4 <romeofts18@gmail.com>
2018-02-20 09:08:59 +0300

    new

--- dz4.php
+++ dz4.php
@@ -2,12 +2,12 @@
 $content = file_get_contents('http://api.openweathermap.org/data/2.5/weather?q=Moscow,Ru&appid=6c532ad7bfe38eeb69dc23edfa8ad4a7') ;
 //echo $content;
 $decoded = json_decode($content, true);
-$temperature = $decoded['main'] ['temp'] - 273;
-$pressure = ($decoded['main'] ['pressure'] / 1000)*750;
-$humidity = $decoded['main'] ['humidity']; 
-$winddeg = $decoded['wind'] ['deg'];
-$windspeed = $decoded['wind'] ['speed'];
-$clouds = $decoded['clouds'] ['all'];
+$temperature = 'Текущая температура:'.  ($decoded['main'] ['temp'] - 273) . ' Градусов Цельсия.';
+$pressure = 'Давление: ' . (($decoded['main'] ['pressure'] / 1000)*750) . ' мм. ртутного столба.';
+$humidity = 'Влажность: ' . ($decoded['main'] ['humidity']) . ' %.'; 
+$winddeg = 'Направление ветра: ' . ($decoded['wind'] ['deg']) . ' градусов,   ';
+$windspeed = 'Скорость ветра: ' . ($decoded['wind'] ['speed']) . ' метров в секунду.';
+$clouds = 'Облачность:  ' . ($decoded['clouds'] ['all']) . ' %.';
 /*echo '<pre>';
 print_r($decoded);*/
 ?>
@@ -20,18 +20,14 @@ print_r($decoded);*/
 <body>
     <div>
     	<h1>Погода:</h1>
-        <p>Текущая температура:    <?= $temperature ?> Градусов Цельсия.</br>
-        	Давление: <?= $pressure ?> мм. ртутного столба.</br> 
-        	Влажность: <?= $humidity ?> %. </br>
-        	 Ветер:</br>   
-        	 Направление:<?= $winddeg ?> градусов, скорость: <?= $windspeed ?> метров в сеrунду.</br>
-        	 Облачность: <?= $clouds ?> %.</p>
+        <p><?= $temperature ?> </br>
+        	<?= $pressure ?></br> 
+        	<?= $humidity ?>  </br>
+        	</br>   
+        	 <?= $winddeg ?><?= $windspeed ?> </br>
+        	 <?= $clouds ?></p>
     </div>
 </body>
-<!--echo 'Текущая температура: ', $temperature , ' Градусов Цельсия.', '</br>', 'Давление: ',$pressure, ' мм. ртутного столба.', '</br>','Влажность:',$humidity, '%.', '</br>';
-echo 'Погода сейчас:', '</br>' ;
-echo '	Ветер:', ' направление: ', $winddeg, ' градусов, скорость: ',$windspeed, ' метров в сеrунду.', '</br>';
-echo '	Облачность:' , $clouds, '%.','</br>';-->
 
 
 
