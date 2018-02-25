cb2212f9067d9ee962fac6b20125762d8e53f2e8
vollva4 <romeofts18@gmail.com>
2018-02-25 14:38:23 +0300

    last

--- dz4.php
+++ dz4.php
@@ -1,15 +1,17 @@
 <?php 
-$content = file_get_contents('http://api.openweathermap.org/data/2.5/weather?q=Moscow,Ru&appid=6c532ad7bfe38eeb69dc23edfa8ad4a7') ;
+$id = "appid=6c532ad7bfe38eeb69dc23edfa8ad4a7";
+$city = 'Moscow,Ru';
+$content = file_get_contents('http://api.openweathermap.org/data/2.5/weather?q=$city&$id') ;
 //echo $content;
 $decoded = json_decode($content, true);
 $temperature = 'Текущая температура:'.  ($decoded['main'] ['temp'] - 273) . ' Градусов Цельсия.';
 $pressure = 'Давление: ' . (($decoded['main'] ['pressure'] / 1000)*750) . ' мм. ртутного столба.';
 $humidity = 'Влажность: ' . ($decoded['main'] ['humidity']) . ' %.'; 
-$winddeg = 'Направление ветра: ' . ($decoded['wind'] ['deg']) . ' градусов,   ';
+//$winddeg = 'Направление ветра: ' . ($decoded['wind'] ['deg']) . ' градусов,   ';
 $windspeed = 'Скорость ветра: ' . ($decoded['wind'] ['speed']) . ' метров в секунду.';
 $clouds = 'Облачность:  ' . ($decoded['clouds'] ['all']) . ' %.';
-/*echo '<pre>';
-print_r($decoded);*/
+//echo '<pre>';
+//print_r($decoded);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -24,7 +26,7 @@ print_r($decoded);*/
         	<?= $pressure ?></br> 
         	<?= $humidity ?>  </br>
         	</br>   
-        	 <?= $winddeg ?><?= $windspeed ?> </br>
+        	 <?= $windspeed ?> </br>
         	 <?= $clouds ?></p>
     </div>
 </body>
